fd7dd05f9f24eb05aa5594c077ec3650593c7be6
Matheus <Matheusaraujo220@gmail.com>
2024-09-10 21:46:32 -0300

    Metodo Falsa Posição

--- Metodos/Metodo_FalsaPosicao.jl
+++ Metodos/Metodo_FalsaPosicao.jl
@@ -1,19 +1,33 @@
-function falsa_posicao(a, b, fa, fb)
-    return a - fa * (b - a) / (fb - fa)
+using Printf
+
+# Definindo a função
+function f(x)
+    return (x + 1) * (x - 1) * (x - 3)
 end
 
-m = falsa_posicao(a, b, f(a), f(b))
+# Definindo a função de falsa posição
+function falsa_posicao(a, b)
+    return (a * f(b) - b * f(a)) / (f(b) - f(a))
+end
+
+# Definindo os valores iniciais
+a = -4
+b = 4
+tol = 0.01
+
+# Calculando a primeira aproximação
+m = falsa_posicao(a, b)
 
 println("\nMétodo da Falsa Posição: \n")
 while abs(f(m)) > tol
     if f(a) * f(m) < 0
-       global  b = m
+       global b = m
     elseif f(b) * f(m) < 0
-       global  a = m
+       global a = m
     else
         break
     end
-   global  m = falsa_posicao(a, b, f(a), f(b))
+   global m = falsa_posicao(a, b)
 end
 
 @printf("\nFinalmente raiz = %.5f\n", m)
